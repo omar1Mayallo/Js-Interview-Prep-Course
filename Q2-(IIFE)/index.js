033d259bd57f36c19cff2cd11a9b3a60d2bc33ea
omar mayallo <omar.mayallo4@gmail.com>
2023-03-20 12:10:27 +0200

    Question 2

---
+++ Q2-(IIFE)/index.js
@@ -0,0 +1,26 @@
+//___________QUESTION___________//
+// What is IIFE and why use it?
+
+//___________BEFORE_SOLVE_REFRESH___________//
+//_immediately invoked function expressions-[Functions]
+/*
+ * An IIFE (Immediately Invoked Function Expression) is a JavaScript      function that runs as soon as it is defined.
+ */
+
+//_Resources
+/*
+[https://www.javascripttutorial.net/javascript-immediately-invoked-function-expression-iife/]
+[https://developer.mozilla.org/en-US/docs/Glossary/IIFE]
+*/
+
+//___________MY_SOLVE___________//
+// ...
+
+////--------------------------------------------------////
+////----------MARKED-[MY_SOLVE]-[Success ✔]----------////
+////-------------------------------------------------////
+
+//___________INSTRUCTOR_SOLVE___________//
+// ...
+//___________AFTER_INSTRUCTOR_SOLVE_LEARN___________//
+// ...
